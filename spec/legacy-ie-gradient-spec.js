0835044c1653c2377d8088f61a80520e63ccdb44
Matt Vanderpol <matt@vanderpol.net>
2014-09-27 12:33:38 -0700

    Initial work.

---
+++ spec/legacy-ie-gradient-spec.js
@@ -0,0 +1,22 @@
+var legacyIEGradient = require('../lib/legacy-ie-gradient');
+var css = '.rule { background: linear-gradient(to top, #000, #fff); }';
+
+//console.log(legacyIEGradient);
+
+describe('legacyIEGradient', function() {
+
+  describe('with default settings', function() {
+
+    it('generates IE Gradient', function() {
+
+    //-ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorstr=#fad59f, endColorstr=#fa9907)";
+      var expected = '.rule { background: linear-gradient(to top, #000, #fff); filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=#000, endColorstr=#fff); }';
+      var processed = legacyIEGradient.process(css);
+
+      expect(processed).toBe(expected);
+    });
+
+  });
+
+});
+
